48efa0537e98874be1c422c9926b2beec5293845
Luis <Luis@LAPTOP-0EFHT9GS>
2023-01-10 08:23:12 -0500

    fix: :bug: Change path of storybook resourse from docs to pipe-labs-components in the fixer script.
    GH Pages uses `pipe-labs-components/assets/` path to load all assets in storybook  instead of `docs/assets` used in build local version.`

--- fixer.js
+++ fixer.js
@@ -1,36 +1,20 @@
 import { createReadStream, renameSync, appendFileSync } from 'fs'
 import { exec, execSync } from 'child_process'
+import yargs from 'yargs/yargs'
+
+const { argv } = yargs(process.argv.slice(2)).option('e', {
+  alias: 'environment',
+  type: 'string',
+})
+
+const assetsPath =
+  argv.e === 'gh-pages' ? '/pipe-labs-components/assets' : '/docs/assets'
 
 renameSync('./docs/iframe.html', './docs/iframeToDelete.html')
 const stream = createReadStream('./docs/iframeToDelete.html')
 stream.on('data', (chunk) => {
   const data = chunk.toString()
-  const correctData = data.replace(/\/assets/g, '/docs/assets')
+  const correctData = data.replace(/\/assets/g, assetsPath)
   appendFileSync('./docs/iframe.html', correctData)
   execSync('yarn clean:removeIframe')
 })
-
-/*
-
-
-
-
-
-stream.on('close', (data) => {
-  console.log('No hay mas')
-  console.log({ data })
-})
-*/
-/*
-exec('yarn --version', (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`)
-    return
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`)
-    return
-  }
-  console.log(`stdout: ${stdout}`)
-})
-*/
